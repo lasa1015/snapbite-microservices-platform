3072431e9baba9b227c680d68604a0c0c60ea01d
Sha Luo <luosha1015@gmail.com>
2025-05-02 09:46:11 +0100

    feat: connected backend and RDS

--- backend/src/main/java/com/shaluo/snapbite/BackendApplication.java
+++ backend/src/main/java/com/shaluo/snapbite/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.shaluo.snapbite;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		// 检查系统环境变量DB_HOST 是否存在
+		if (System.getenv("DB_HOST") == null) {
+
+			// 如果系统变量DB_HOST没有，说明是本地开发，需要手动加载 .env 文件
+			// 如果存在，是生产环境，依靠docker compose up文件注入
+			Dotenv dotenv = Dotenv.configure()
+					.directory("./backend") // 指定本地 .env 文件所在目录
+					.filename(".env.dev-local")
+					.load();
+
+			// 将变量设置到系统属性中，Spring Boot 才能识别 ${}
+			System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+			System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+			System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+			System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		}
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
