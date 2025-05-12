402897fe2d563c5f9fec6ce777feeaf3198879d4
Sha Luo <luosha1015@gmail.com>
2025-05-12 17:30:47 +0100

    chore: update build platforms to linux/amd64 and include workflow file in trigger paths

---
+++ docker/pull-images.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 写死用户名
+DOCKER_USERNAME="lasa1015"
+
+# 镜像名列表
+IMAGES=(
+  snapbite-user-service
+  snapbite-restaurant-service
+  snapbite-cart-service
+  snapbite-order-service
+  snapbite-nginx
+  snapbite-eureka
+  snapbite-gateway
+  snapbite-mongo
+)
+
+# 拉取镜像
+for IMAGE in "${IMAGES[@]}"; do
+  echo "🚀 Pulling image: ${DOCKER_USERNAME}/${IMAGE}:latest"
+  docker pull ${DOCKER_USERNAME}/${IMAGE}:latest
+done
+
+echo "✅ All images pulled successfully."
